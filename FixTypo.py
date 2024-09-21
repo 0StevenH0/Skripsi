43e7097e923f8f955f19741c4d1dbcc216ac056d
Matoola <mauthus23@gmail.com>
2024-09-22 01:20:45 +0700

    symspell

--- FixTypo.py
+++ FixTypo.py
@@ -1,16 +1,32 @@
-import torch
-import transformers
-from transformers import pipeline
+from symspellpy import SymSpell, Verbosity
+import os
 
-# TODO
-"""use pretrained library to remove typos, then return it"""
-class typo_corrector():
-    def __init__(self, model_path="./typo_corrector"):
-        self.corrector = pipeline("text2text-generation", model=model_path)
+class typo_corrector:
+    def __init__(self,
+                 prefix_length=7, 
+                 dictionary_path="typo_corrector/frequency_dict.txt",
+                 term_index=0, 
+                 count_index=1):
 
-    def correct_text(self, text: str, max_length: int = 512) -> str:
-        corrected = self.corrector(text, max_length=max_length)
+        self.sym_spell = SymSpell(prefix_length=prefix_length)
 
-        corrected_text = corrected[0]['generated_text']
+        if not os.path.exists(dictionary_path):
+            raise FileNotFoundError(f"Frequency dictionary file not found at {dictionary_path}")
 
+        if not self.sym_spell.load_dictionary(dictionary_path, term_index, count_index):
+            raise ValueError(f"Failed to load dictionary from {dictionary_path}")
+
+    def fix(self, text: str) -> str:
+        corrected_words = []
+        for word in text.split():
+            corrected_word = self.fix(word)
+            corrected_words.append(corrected_word)
+        corrected_text = ' '.join(corrected_words)
         return corrected_text
+
+    def fix_word(self, word: str) -> str:
+        suggestions = self.sym_spell.lookup(word, Verbosity.CLOSEST, max_edit_distance=2)
+        if suggestions:
+            return suggestions[0].term
+        else:
+            return word
